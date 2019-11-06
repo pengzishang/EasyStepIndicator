4ab16c0db6676a84245f08d2d4c66df8a0690317
Desh <pengzishang@163.com>
2019-11-06 18:12:54 +0800

    重构代码,可以分别定义每个元素的属性 (#16)
    * Update readme,add English support (#9)
    
    * update_readme
    
    Signed-off-by: pengzishang <pengzishang@163.com>
    
    * add link
    
    Signed-off-by: pengzishang <pengzishang@163.com>
    
    * Set theme jekyll-theme-minimal
    
    * Fix a bug caused by a screen rotation (#11)
    
    Signed-off-by: pengzishang <pengzishang@163.com>
    
    * Fix a bug caused by a screen rotation (#11)
    
    Signed-off-by: pengzishang <pengzishang@163.com>
    
    * Update README.md
    
    * Update HorizontalCodeController.swift
    
    * Update EasyStepIndicator.podspec
    
    * Update EasyStepIndicator.swift
    
    * Update README_EN.md
    
    * refactor code (#15)
    
    * wip day1
    
    Signed-off-by: pengzishang <pengzishang@163.com>
    
    * wip day2
    
    Signed-off-by: pengzishang <pengzishang@163.com>
    
    * wip day3
    
    Signed-off-by: pengzishang <pengzishang@163.com>
    
    * wip day4
    
    Signed-off-by: pengzishang <pengzishang@163.com>
    
    * wip day5
    
    Signed-off-by: pengzishang <pengzishang@163.com>
    
    * bug day 1
    
    Signed-off-by: pengzishang <pengzishang@163.com>
    
    * bug day2
    
    Signed-off-by: pengzishang <pengzishang@163.com>
    
    * 里程碑:横向描述,文字显示正常
    
    Signed-off-by: pengzishang <pengzishang@163.com>
    
    * 里程碑:垂直适配
    
    Signed-off-by: pengzishang <pengzishang@163.com>
    
    * 里程碑:各元素设立独立属性
    
    Signed-off-by: pengzishang <pengzishang@163.com>
    
    * 里程碑:旋转修复
    
    Signed-off-by: pengzishang <pengzishang@163.com>
    
    * 修复纵向起始点问题
    
    Signed-off-by: pengzishang <pengzishang@163.com>
    
    * 纵向 shouldStepLineFitDescriptionText测试成功
    
    Signed-off-by: pengzishang <pengzishang@163.com>
    
    * bug day 2
    
    Signed-off-by: pengzishang <pengzishang@163.com>
    
    * wip demo
    
    Signed-off-by: pengzishang <pengzishang@163.com>
    
    * format
    
    Signed-off-by: pengzishang <pengzishang@163.com>
    
    * 里程碑:全部测试完毕
    
    Signed-off-by: pengzishang <pengzishang@163.com>
    
    * demo code
    
    Signed-off-by: pengzishang <pengzishang@163.com>
    
    
    重构代码,可以分别定义每个元素的属性

--- EasyStepIndicator/DescriptionTextLayer.swift
+++ EasyStepIndicator/DescriptionTextLayer.swift
@@ -9,75 +9,78 @@
 import UIKit
 
 class DescriptionTextLayer: CAShapeLayer {
-
-    private let tintTextLayer = CATextLayer()
-    //步骤描述文字
-    public var stepDescriptionText: String = ""
-
-    //步骤描述文字未完成时候颜色
-    public var stepDescriptionTextIncompleteColor: UIColor = UIColor.red
-
-    //步骤描述文字完成时候颜色
-    public var stepDescriptionTextCompleteColor: UIColor = UIColor.green
-
-    //步骤描述文字的大小
-    public var stepDescriptionTextFontSize: CGFloat = 18
-
-    var currentStepAsIncomplete = false
-
-    var isCurrent: Bool = false {
-        didSet {
-            self.updateStatus()
-        }
-    }
-
-    var isFinished: Bool = false {
-        didSet {
-            self.updateStatus()
-        }
-    }
-
-    fileprivate func drawText() {
-        self.tintTextLayer.removeFromSuperlayer()
-        self.tintTextLayer.string = stepDescriptionText
-//        self.tintTextLayer.
-        self.tintTextLayer.alignmentMode = .center
-        self.tintTextLayer.font = UIFont.systemFont(ofSize: self.stepDescriptionTextFontSize)
-        self.tintTextLayer.fontSize = self.stepDescriptionTextFontSize
-        self.tintTextLayer.contentsScale = UIScreen.main.nativeScale
-        self.tintTextLayer.backgroundColor = UIColor.clear.cgColor
-        self.tintTextLayer.frame = self.bounds.integral
-        self.tintTextLayer.zPosition = 1000
-        self.tintTextLayer.isWrapped = true
-    }
-
-    func updateStatus() {
-        guard stepDescriptionText.count > 0 else {
-            return
-        }
-        self.drawText()
-        if isFinished {
-            self.tintTextLayer.foregroundColor = stepDescriptionTextCompleteColor.cgColor
-        } else if isCurrent {
-            self.tintTextLayer.foregroundColor = currentStepAsIncomplete ?
-                stepDescriptionTextIncompleteColor.cgColor : stepDescriptionTextCompleteColor.cgColor
-        } else {
-            self.tintTextLayer.foregroundColor = stepDescriptionTextIncompleteColor.cgColor
-        }
-        self.addSublayer(self.tintTextLayer)
-    }
-
-    override init() {
-        super.init()
-        self.fillColor = UIColor.clear.cgColor
-    }
-
-    override init(layer: Any) {
-        super.init(layer: layer)
-    }
-
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-
+	
+	private let tintTextLayer = CATextLayer()
+	
+	public var config: TitleConfig?
+	
+	public var indicator: EasyStepIndicator?
+	
+	public var isHorizontal: Bool = true
+	
+	public var titleSize: CGSize?
+	
+	init(titleConfig: TitleConfig, target: EasyStepIndicator) {
+		super.init()
+		self.config = titleConfig
+		self.fillColor = UIColor.clear.cgColor
+		self.indicator = target
+	}
+	
+	
+	required init?(coder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+	
+	override init(layer: Any) {
+		super.init(layer: layer)
+	}
+	
+	fileprivate func drawText() {
+		guard let config = self.config else {
+			assertionFailure("没有指定config")
+			return
+		}
+		
+		self.tintTextLayer.removeFromSuperlayer()
+		self.tintTextLayer.string = config.title.content
+		self.tintTextLayer.alignmentMode = .center
+		self.tintTextLayer.font = UIFont.systemFont(ofSize: config.title.fontSize)
+		self.tintTextLayer.fontSize = config.title.fontSize
+		self.tintTextLayer.contentsScale = UIScreen.main.scale
+		self.tintTextLayer.frame = self.bounds.integral
+		self.tintTextLayer.isWrapped = true
+	}
+	
+	func updateStatus() {
+		
+		guard let indicator = indicator else {
+			assertionFailure("没有指定EasyStepIndicator")
+			return
+		}
+		
+		guard let config = self.config else {
+			assertionFailure("没有指定config")
+			return
+		}
+		
+		guard let _ = config.title.content else {
+			return
+		}
+		
+		self.isHorizontal = indicator.direction == .leftToRight || indicator.direction == .rightToLeft
+		
+		self.drawText()
+		if indicator.currentStep > config.stepIndex {
+			self.tintTextLayer.foregroundColor = config.colors.complete?.cgColor
+		} else if indicator.currentStep == config.stepIndex {
+			self.tintTextLayer.foregroundColor = indicator.currentStepAsIncomplete ?
+				config.colors.incomplete?.cgColor : config.colors.complete?.cgColor
+		} else {
+			self.tintTextLayer.foregroundColor = config.colors.incomplete?.cgColor
+		}
+		self.addSublayer(self.tintTextLayer)
+	}
+	
+	
 }
