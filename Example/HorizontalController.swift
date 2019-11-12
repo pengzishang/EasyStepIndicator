34f21570f986d3b6d2c467deb136bdc1f10c4d95
pengzishang <pengzishang@163.com>
2019-11-12 16:25:20 +0800

    demo done
    Signed-off-by: pengzishang <pengzishang@163.com>

--- Example/HorizontalController.swift
+++ Example/HorizontalController.swift
@@ -13,8 +13,14 @@ class HorizontalController: UIViewController {
     @IBOutlet weak var stepper: UIStepper!
     @IBOutlet weak var indicator: EasyStepIndicator!
     @IBOutlet weak var currentStep: UILabel!
-    @IBOutlet weak var segment: UISegmentedControl!
+    @IBOutlet weak var alignmentSegment: UISegmentedControl!
     @IBOutlet weak var switcher: UISwitch!
+    @IBOutlet weak var directSegment: UISegmentedControl!
+    @IBOutlet weak var minContentMargin: UIStepper!
+    @IBOutlet weak var minContentLab: UILabel!
+    @IBOutlet weak var maxContentWidthLabel: UILabel!
+    @IBOutlet weak var maxContentWidthStepper: UIStepper!
+    
     
     var shouldStepLineFitText = true
     
@@ -23,12 +29,15 @@ class HorizontalController: UIViewController {
         self.indicator.dataSource = self
         self.indicator.delegate = self
         self.indicator.alignmentMode = .top
-        segment.selectedSegmentIndex = 0
+        alignmentSegment.selectedSegmentIndex = 0
+        minContentMargin.value = Double(self.indicator.minContentMargin)
+        minContentLab.text = "\(self.indicator.minContentMargin)"
+        self.maxContentWidthLabel.text = self.indicator.maxContentWidth == CGFloat.greatestFiniteMagnitude ? "Unlimit" : "\(self.indicator.maxContentWidth)"
+        
         switcher.isOn = shouldStepLineFitText
         self.indicator.freezeZone = UIEdgeInsets.init(top: 10, left: 10, bottom: 10, right: 20)
         stepper.maximumValue = Double(indicator.numberOfSteps - 1)
         setState(step: 0)
-        
     }
     
     fileprivate func setState(step: Int) {
@@ -40,8 +49,12 @@ class HorizontalController: UIViewController {
         setState(step: Int(sender.value))
     }
     
-    @IBAction func changeSegmentValue(_ sender: UISegmentedControl) {
+    @IBAction func changeAlignmentSegmentValue(_ sender: UISegmentedControl) {
         indicator.alignmentMode = [AlignmentMode.top,AlignmentMode.center,AlignmentMode.centerWithAnnularStartAndAnnularEnd][sender.selectedSegmentIndex]
+    }
+    
+    @IBAction func didDirectSegmentValueChange(_ sender: UISegmentedControl) {
+        self.indicator.direction = [Direction.leftToRight,Direction.rightToLeft,Direction.topToBottom,Direction.bottomToTop][sender.selectedSegmentIndex]
     }
     
     @IBAction func changeSwitchValue(_ sender: UISwitch) {
@@ -49,15 +62,26 @@ class HorizontalController: UIViewController {
         indicator.reload()
     }
     
+    @IBAction func changeContentMarginValue(_ sender: UIStepper) {
+        self.indicator.minContentMargin = CGFloat(sender.value)
+        minContentLab.text = "\(self.indicator.minContentMargin)"
+    }
+    
+    @IBAction func maxContentWidthStepperValueChange(_ sender: UIStepper) {
+        self.maxContentWidthLabel.text = sender.value == 0 ? "Unlimit" : "\(sender.value)"
+        self.indicator.maxContentWidth = sender.value == 0 ? CGFloat.greatestFiniteMagnitude : CGFloat(sender.value)
+    }
+    
     func randomColor() -> UIColor {
         return UIColor.init(red:CGFloat(arc4random_uniform(255))/CGFloat(255.0), green:CGFloat(arc4random_uniform(255))/CGFloat(255.0), blue:CGFloat(arc4random_uniform(255))/CGFloat(255.0) , alpha: 1)
     }
 }
 
+
 extension HorizontalController:EasyStepIndicatorDataSource {
     func characterForStep(indicator: EasyStepIndicator, index: Int) -> String {
         return "A"
-        ["A","B","C","D"][index]
+//        ["A","B","C","D"][index]
     }
     
     func titleForStep(indicator: EasyStepIndicator, index: Int) -> String {
